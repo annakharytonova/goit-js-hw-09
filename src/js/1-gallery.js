5581dba995033cbff1c6d5c61c64857087d70150
Anna Kharytonova <haritonova1512@gmail.com>
2024-11-18 20:21:29 +0200

    begin

---
+++ src/js/1-gallery.js
@@ -0,0 +1,15 @@
+console.log("Gallery");
+
+
+
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt', // Підпис з атрибута alt
+//   captionDelay: 250, // Затримка в мілісекундах
+// });
+
+// new SimpleLightbox('.some-element a', {
+//   /* options */
+// });
